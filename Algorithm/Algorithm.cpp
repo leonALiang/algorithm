c3a43745855d3ba608630f77fd306882e29b484e
Liang Cui <liang.cui@sas.com>
2014-05-06 15:40:23 +0800

    add new algorithms

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -29,4 +29,66 @@ namespace algorithm
 
 		return count;
 	}
+
+	int findKthMaxElement(int* arr, int n, int k)
+	{
+
+	}
+
+	int MaxSubsequenceSum(const int A[], int N)
+	{
+		int thisSum, maxSum, j;
+		thisSum = maxSum = 0;
+		for(j = 0; j < N; j++)
+		{
+			thisSum += A[j];
+			if(thisSum > maxSum)
+				maxSum = thisSum;
+			else if(thisSum < 0)
+				thisSum = 0;
+		}
+
+		return maxSum;
+	}
+
+	int BinarySearch(const int A[], int N, int x)
+	{
+		int low, mid, high;
+		low = 0; high = N - 1;
+		while (low <= high)
+		{
+			mid = (low + high) / 2;
+			if(A[mid] < x)
+				low = mid + 1;
+			else if(A[mid] > x)
+				high = mid - 1;
+			else
+				return mid;
+		}
+
+		return -1;
+	}
+
+	unsigned int Gcd(unsigned int M, unsigned int N)
+	{
+		unsigned int Rem;
+		while(N > 0)
+		{
+			Rem = M % N;
+			M = N;
+			N = Rem;
+		}
+
+		return M;
+	}
+
+	long int Pow(long int X, unsigned int N)
+	{
+		if( N ==0 ) return 1;
+		if(N == 1) return N;
+		if(N%2 == 0)
+			return Pow(X*X, N/2);
+		else
+			return Pow(X*X, N/2) * X;
+	}
 }
